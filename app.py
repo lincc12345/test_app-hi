51a34d58b504ed41cfc14c22aa31f2d338925b77
Chen Lin <cl34@loan117048i.internal.sanger.ac.uk>
2025-07-04 15:12:04 +0100

    yooo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("helllo how are you")
+st.title("wellcomeeeeeeeeeeeee to my website pay 100 rn")
 import streamlit as st
 
 audio_value = st.audio_input("Record a voice message")
@@ -7,6 +7,18 @@ audio_value = st.audio_input("Record a voice message")
 if audio_value:
     st.audio(audio_value)
     import streamlit as st
+
+import streamlit as st
+
+st.button("Reset", type="primary")
+if st.button("ciallllloooo~~~"):
+    st.write("ciallllloooo~~~")
+else:
+    st.write("ciallllloooo~~~")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("ciallllloooo~~~")
+
 
 import streamlit as st
 
@@ -23,3 +35,4 @@ st.badge("NO", icon=":material/check:", color="red")
 st.markdown(
     ":violet-badge[:material/star: NO] :orange-badge[⚠️ NOO] :gray-badge[NO]"
 )
+
